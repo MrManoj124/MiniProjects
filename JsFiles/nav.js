60aca3f12c84ae856a79b22651a879bcd38bbd65
MrManoj124 <manojmanorooban@gmail.com>
2026-01-29 13:44:43 +0530

    create nav.js file

---
+++ JsFiles/nav.js
@@ -0,0 +1,10 @@
+// Optional JS (not required for CSS animation)
+// You can add button actions here
+
+document.querySelector(".cart").addEventListener("click", () => {
+  alert("Added to cart!");
+});
+
+document.querySelector(".checkout").addEventListener("click", () => {
+  alert("Proceeding to checkout!");
+});
